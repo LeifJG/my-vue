2f1d9acc77344dc651f4cfc82b0d01df6a7e7aba
lijg <lijg@yunjiglobal.com>
2020-07-08 22:46:16 +0800

    30%

--- myvue.js
+++ myvue.js
@@ -2,7 +2,46 @@ class Compile {
   constructor(el, vm) {
     this.el = this.isElementNode(el) ? el : document.querySelector(el)
     this.vm = vm
+    const fragment = this.node2Fargment(this.el)
+    this.compile(fragment)
+    this.el.appendChild(fragment)
   }
+
+  compile(fragment) {
+    const { childNodes } = fragment;
+    [...childNodes].forEach((child) => {
+      if (this.isElementNode(child)) {
+        // 元素
+        this.compileElement(child)
+      } else {
+        // 文本
+        this.compileText(child)
+      }
+
+      if (child.childNodes && child.childNodes.length) {
+        this.compile(child)
+      }
+    })
+  }
+
+  compileElement(node) {
+
+
+  }
+
+  compileText(text) {
+    
+  }
+
+  node2Fargment(el) {
+    const f = document.createDocumentFragment()
+    let firstChild
+    while (firstChild = el.firstChild) {
+      f.appendChild(firstChild)
+    }
+    return f
+  }
+
   isElementNode(node) {
     return node.nodeType === 1
   }
